b71894e8467e331ae1707468f20c4b77cdcda36f
tihitGN <seblemichalew@gmail.com>
2022-12-09 12:19:23 -0500

    first commit

---
+++ JavaScriptassement/person.js
@@ -0,0 +1,9 @@
+const fname = document.getElementById('fame')
+const lname = document.getElementById('lname')
+const age = document.getElementById('age')
+const age = document.getElementById('form')
+
+form.addEventListener(submit',(e) => {
+    let mes
+    e.preventDefault()
+})
